239987b8f28337c8e9ead70eb0e183d51c91e003
Raman Singh <mail.raman.s@gmail.com>
2020-04-08 18:49:00 +0200

    Initial commit

---
+++ src/main/java/com/github/ramannbg/maprdbspringdatasample/MaprdbSpringDataSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.ramannbg.maprdbspringdatasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaprdbSpringDataSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaprdbSpringDataSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ramannbg/maprdbspringdatasample/MaprdbSpringDataSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ramannbg.maprdbspringdatasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaprdbSpringDataSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
